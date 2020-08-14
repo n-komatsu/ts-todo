449aeded385167d33ccc18b10d7d0fee9ba0ac5a
n-komatsu <komatsu@gizumo-inc.jp>
2020-08-14 18:33:04 +0900

    add: todosテーブルのマイグレーションファイルをcliからgenerete

---
+++ ts-todo/src/migrations/1597397066232-CreateTodosTable.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateTodosTable1597397066232 implements MigrationInterface {
+    name = 'CreateTodosTable1597397066232'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query("CREATE TABLE `todo` (`id` int NOT NULL AUTO_INCREMENT, `title` varchar(255) NOT NULL, `completed` tinyint NOT NULL DEFAULT 0, PRIMARY KEY (`id`)) ENGINE=InnoDB");
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query("DROP TABLE `todo`");
+    }
+
+}
