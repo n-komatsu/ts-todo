7f531402d7578e85324b46c475251938aeca54dd
n-komatsu <komatsu@gizumo-inc.jp>
2020-08-14 18:33:55 +0900

    update: CREATE TABLE `todo` -> `todos`

--- ts-todo/src/migrations/1597397066232-CreateTodosTable.ts
+++ ts-todo/src/migrations/1597397066232-CreateTodosTable.ts
@@ -4,11 +4,11 @@ export class CreateTodosTable1597397066232 implements MigrationInterface {
     name = 'CreateTodosTable1597397066232'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("CREATE TABLE `todo` (`id` int NOT NULL AUTO_INCREMENT, `title` varchar(255) NOT NULL, `completed` tinyint NOT NULL DEFAULT 0, PRIMARY KEY (`id`)) ENGINE=InnoDB");
+        await queryRunner.query("CREATE TABLE `todos` (`id` int NOT NULL AUTO_INCREMENT, `title` varchar(255) NOT NULL, `completed` tinyint NOT NULL DEFAULT 0, PRIMARY KEY (`id`)) ENGINE=InnoDB");
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("DROP TABLE `todo`");
+        await queryRunner.query("DROP TABLE `todos`");
     }
 
 }
