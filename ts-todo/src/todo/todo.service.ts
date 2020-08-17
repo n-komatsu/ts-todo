2e59ff265bf55ff80c37b4dcff7d5cb0236e2559
n-komatsu <komatsu@gizumo-inc.jp>
2020-08-17 17:41:27 +0900

    update: TodoServiceにfindAllメソッド追加

--- ts-todo/src/todo/todo.service.ts
+++ ts-todo/src/todo/todo.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Todo } from './todo.entity';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(
+    @InjectRepository(Todo)
+    private todoRepository: Repository<Todo>,
+  ){}
+
+  async findAll(): Promise<Todo[]> {
+    return this.todoRepository.find();
+  }
+}
