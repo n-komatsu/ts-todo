1732dcfa9f6e56f1bfaa31cc9fdaa401dc724ba8
n-komatsu <komatsu@gizumo-inc.jp>
2020-08-19 15:47:37 +0900

    add: ProgressController

--- ts-todo/src/progress/progress.module.ts
+++ ts-todo/src/progress/progress.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ProgressController } from './progress.controller';
 
-@Module({})
+@Module({
+  controllers: [ProgressController]
+})
 export class ProgressModule {}
