29012d481b95b4f43604fde0d23242f83be1537a
n-komatsu <komatsu@gizumo-inc.jp>
2020-08-19 15:50:06 +0900

    update: toggleProgressState

--- ts-todo/src/app.controller.ts
+++ ts-todo/src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api/progress')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post(':id')
+  toggleProgressState(): Object {
+    return {
+      status: 200,
+    }
   }
 }
