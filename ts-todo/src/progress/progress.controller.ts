3cd4e259f50830c4fd998fc364f71ce50e5aff80
n-komatsu <komatsu@gizumo-inc.jp>
2020-08-27 20:21:42 +0900

    update: progress更新メソッド実装

--- ts-todo/src/progress/progress.controller.ts
+++ ts-todo/src/progress/progress.controller.ts
@@ -1,10 +1,15 @@
 import { Controller, Param, Post } from '@nestjs/common';
+import { ProgressService } from './progress.service';
 
 @Controller('api/progress')
 export class ProgressController {
-  @Post(':id')
-  toggleProgressState(@Param() params): Object {
-    const { id } = params.id;
+  constructor(
+    private progressService: ProgressService
+  ){}
+  @Post(':todoId')
+  async toggleProgressState(@Param() params): Promise<Object> {
+    const { todoId } = params;
+    const progress = await this.progressService.update(todoId);
     return {
       status: 200,
     }
--- ts-todo/src/progress/progress.service.ts
+++ ts-todo/src/progress/progress.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Repository, Connection } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Progress } from './progress.entity';
 
 @Injectable()
-export class ProgressService {}
+export class ProgressService {
+  constructor(
+    @InjectRepository(Progress)
+    private progressRepository: Repository<Progress>,
+    private connection: Connection
+  ){}
+
+  async update(id: string) {
+    const progress = await this.progressRepository.findOne({ where: { todo: { id } } });
+    await this.progressRepository.update(progress.id, { completed: !progress.completed });
+    const updateProgress = await this.progressRepository.findOne(progress.id);
+    return updateProgress;
+  }
+}
